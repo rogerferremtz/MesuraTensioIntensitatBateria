3709d49bbac45197245fea99aab6eec3b78bc108
1485604 <71752232+1485604@users.noreply.github.com>
2020-12-03 09:05:34 +0100

    Update mesurafont.c

--- mesurafont.c
+++ mesurafont.c
@@ -491,7 +491,7 @@ int cridarsql(float tensio, float intensitat, int id_tensio, int id_intensitat) 
 	rc = sqlite3_exec(db, sql_tensio, 0, 0, &zErrMsg);
 
 
-	sprintf(cadena_URI, "/cloud/guardar_dades.php?id_sensor=1&valor=%f&temps=%02d-%02d-%02d+%02d%%3A%02d%%3A%02d", tensio, any, mes, dia, hores, minuts, segons);
+	sprintf(cadena_URI, "/cloud/guardar_dades.php?id_sensor=64&valor=%f&temps=%02d-%02d-%02d+%02d%%3A%02d%%3A%02d", tensio, any, mes, dia, hores, minuts, segons);
 
 	http_get(nom_servidor, cadena_URI);
 
@@ -517,7 +517,7 @@ int cridarsql(float tensio, float intensitat, int id_tensio, int id_intensitat) 
 
 
 
-	sprintf(cadena_URI, "/cloud/guardar_dades.php?id_sensor=1&valor=%f&temps=%02d-%02d-%02d+%02d%%3A%02d%%3A%02d", intensitat, any, mes, dia, hores, minuts, segons);
+	sprintf(cadena_URI, "/cloud/guardar_dades.php?id_sensor=65&valor=%f&temps=%02d-%02d-%02d+%02d%%3A%02d%%3A%02d", intensitat, any, mes, dia, hores, minuts, segons);
 
 	http_get(nom_servidor, cadena_URI);
 
@@ -595,7 +595,7 @@ int main (int argc, char *argv[])
 		}
 	}
 
-	printf("Directori: %s, Servidor: %s.\n", directori_basedades, nom_servidor);
+	printf("\n\nDirectori: %s, Servidor: %s.\n\n", directori_basedades, nom_servidor);
 
 
 	sqlite3 *db;
